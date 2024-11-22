a7ad5db6f7e7db3bfc301afcdb11f9eb60681562
lucky <lucky@wemakeprice.com>
2024-11-22 14:02:33 +0900

    Initial Commit

---
+++ src/test/java/kr/co/kwt/elements/ElementsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kwt.elements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElementsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
